9a76507f9555c120e77189b833c6fc846aac7ec6
Ritz <ritsrivastava@gmail.com>
2021-12-01 16:44:00 +0100

    bild fix

--- pages/new-meetup/index.jsx
+++ pages/new-meetup/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MeetupForm from "../../components/MeetupForm";
 import { useRouter } from "next/router";
 
-export default function index() {
+export default function NewMeetupPage() {
   const router = useRouter();
   const submitHandler = async (newMeetup) => {
     const resp = await fetch("/api/new-meetup", {
